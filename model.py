8db2f776999e8e94cde7b259e643f3563b3bc31d
WA_automat <1577696824@qq.com>
2024-02-25 19:18:39 +0800

    回退版本

--- model.py
+++ model.py
@@ -8,12 +8,8 @@ class LSTM_with_Attention(nn.Module):
         super(LSTM_with_Attention, self).__init__()
         self.embedding = nn.Embedding(vocab_size, embedding_dim)
         self.lstm = nn.LSTM(embedding_dim, hidden_dim, num_layers=num_layers, dropout=dropout, batch_first=True)
-        self.bn1 = nn.BatchNorm1d(hidden_dim)  # 添加批标准化层
-        self.dropout1 = nn.Dropout(dropout)
-        self.bn2 = nn.BatchNorm1d(hidden_dim)  # 添加批标准化层
-        self.dropout2 = nn.Dropout(dropout)
-        self.bn3 = nn.BatchNorm1d(hidden_dim)  # 添加批标准化层
-        self.dropout3 = nn.Dropout(dropout)
+        self.bn = nn.BatchNorm1d(hidden_dim)  # 添加批标准化层
+        self.dropout = nn.Dropout(dropout)
         self.fc = nn.Linear(hidden_dim, output_dim)
         self.attention = nn.Linear(hidden_dim, 1)  # 注意力机制
 
@@ -22,28 +18,19 @@ class LSTM_with_Attention(nn.Module):
         inputs = self.embedding(inputs)
         lstm_out, _ = self.lstm(inputs)
 
-        lstm_out = self.bn1(lstm_out.permute(0, 2, 1)).permute(0, 2, 1)
-        lstm_out = F.relu(lstm_out)
-        lstm_out = self.dropout1(lstm_out)
-
         # 应用注意力机制
         attention_scores = self.attention(lstm_out).squeeze(-1)
         attention_weights = F.softmax(attention_scores, dim=1).unsqueeze(2)
         context_vector = torch.sum(attention_weights * lstm_out, dim=1)
 
         # Batch Normalization层：加速收敛
-        x = self.bn2(context_vector)
+        x = self.bn(context_vector)
         x = F.relu(x)
-        x = self.dropout2(x)
+        x = self.dropout(x)
 
         # 定义残差连接
         residual = lstm_out[:, -1, :]  # 选择最后一个时间步的输入作为残差项
         x = x + residual  # 将残差项与当前结果相加
-
-        # Batch Normalization层：加速收敛
-        x = self.bn3(x)
-        x = F.relu(x)
-        x = self.dropout3(x)
 
         # 全连接层
         x = self.fc(x)
